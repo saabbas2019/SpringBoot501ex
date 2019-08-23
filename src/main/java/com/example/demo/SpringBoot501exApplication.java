b6465214b4062011fa6fa9a305e086035da34b68
GBTC405007UR <GBTC405007UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-08-23 10:26:31 -0400

    lesson needs to for exercise

---
+++ src/main/java/com/example/demo/SpringBoot501exApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot501exApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot501exApplication.class, args);
+    }
+
+}
